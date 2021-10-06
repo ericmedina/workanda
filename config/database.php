01156799029c29cfd7e9f8fa7923fa1d4fcba685
Eric <eriic.eme@hotmail.com>
2021-10-06 12:10:44 -0300

    404

--- config/database.php
+++ config/database.php
@@ -1,8 +1,8 @@
 <?php
   return [
-    'HOST' => 'td5l74lo6615qq42.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    'HOST' => 'localhost',
     'PORT' => '3306',
-    'DATABASE' => 'g94q46z4brzzbhzl',
-    'USERNAME' => 'yo270hrkjs16aljo',
-    'PASSWORD' => 'eqeru3eacukrzlno'
+    'DATABASE' => 'example',
+    'USERNAME' => 'root',
+    'PASSWORD' => ''
   ];
