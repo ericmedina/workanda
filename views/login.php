d259416cb911e6c1246ba4771546c8622e32180f
Eric <eriic.eme@hotmail.com>
2021-10-06 12:38:20 -0300

    correcciones

--- views/login.php
+++ views/login.php
@@ -1,14 +1,14 @@
   <div class="content-login">
     <div class="card-login">
       <h2 class="card-title">Iniciar sesión</h2>
-      <form action="<?php echo url('/login')?>" method="POST" autocomplete="off">
+      <form action="<?php echo url('/login')?>" method="POST">
         <div class="input-group">
           <label for="email">Email</label>
-          <input type="email" name="email" id="email" placeholder="Ingrese su email" autocomplete="off">
+          <input type="email" name="email" id="email" placeholder="Ingrese su email" >
         </div>
         <div class="input-group">
           <label for="password">Contraseña</label>
-          <input type="password" name="password" id="password" placeholder="Ingrese su contraseña" autocomplete="new-password" inputmode="email">
+          <input type="password" name="password" id="password" placeholder="Ingrese su contraseña" inputmode="email">
         </div>
         <div class="justify-end">
           <button class="btn btn-primary" type="submit">Ingresar</button>
