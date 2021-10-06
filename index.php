bb7428e0a1f5345e85cce16a50a8c3e92ce7f8b5
Eric <eriic.eme@hotmail.com>
2021-10-06 11:50:46 -0300

    Heroku

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ $router->get('/crear-tabla', function () {
   $conn = require_once path('/helpers/Connection.php');
   $tables = $conn->query('SHOW TABLES LIKE "usuarios"');
   if (!$tables->num_rows) {
-    $sql = "CREATE TABLE `workanda`.`usuarios` ( `id` INT(11) NOT NULL AUTO_INCREMENT ,  `nombre` VARCHAR(255) NOT NULL ,  `apellido` VARCHAR(255) NOT NULL ,  `email` VARCHAR(255) NOT NULL ,  `password` VARCHAR(255) NOT NULL ,    PRIMARY KEY  (`id`)) ENGINE = InnoDB;";
+    $sql = "CREATE TABLE `usuarios` ( `id` INT(11) NOT NULL AUTO_INCREMENT ,  `nombre` VARCHAR(255) NOT NULL ,  `apellido` VARCHAR(255) NOT NULL ,  `email` VARCHAR(255) NOT NULL ,  `password` VARCHAR(255) NOT NULL ,    PRIMARY KEY  (`id`)) ENGINE = InnoDB;";
     if (!$conn->query($sql)) {
       echo "Error: " . $sql . "<br>" . $conn->error;
       return;
@@ -35,4 +35,5 @@ $router->get('/crear-tabla', function () {
       return;
     }
   }
+  redirect('/');
 });
